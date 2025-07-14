e325aeba662fccaaffe4bc0176b7e050394a2784
chintan dankhara <chintandankhara9@gmail.com>
2025-07-14 09:17:42 +0530

    A

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
+// api/index.js
 import express from "express";
-// import { Server } from "socket.io";
-import {createServer} from 'http'
-
-const port = 3000;
+import { createServer } from "http";
+import { default as serverless } from "serverless-http";
 
 const app = express();
-const server = createServer(app);
-// const io = new Server(server);
-
-// io.on("connection", (socket) => {
-
-//   socket.on('chat',(msg)=>{
-//     console.log(msg);
-//     socket.except("101").emit("main",msg);
-//   });
-  
-//   socket.on('disconnect',()=>{
-//     console.log('user disconnected');
-//   })
-  
-// });
 
 app.get("/", (req, res) => {
   res.send("Hello World !");
 });
 
-server.listen(port, () => {
-  console.log(`http://localhost:${port}`);
-});
+export default serverless(app);
